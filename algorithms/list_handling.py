05618fe737ccb8462caeee93ac613fe8ad66291e
nicor88 <ncorda@babbel.com>
2017-08-27 22:34:30 +0200

    clean up

--- algorithms/list_handling.py
+++ algorithms/list_handling.py
@@ -9,18 +9,15 @@ print(l_reversed)
 
 # Left rotation
 d = [1, 2, 3, 4, 5]
-d_deque = collections.deque(d)
-# rotate left, use positive to rotate right
-d_deque.rotate(2*-1)
-d_rotated = list(d_deque)
-
-print(d)
-print(d_rotated)
 
 
-def array_left_rotation(a, n, k):
+def array_left_rotation(a, rotation):
     a_deque = collections.deque(a)
-    left_rotation = n * -1
+    left_rotation = rotation * -1
     a_deque.rotate(left_rotation)
     a_rotated = list(a_deque)
     return a_rotated
+
+d_rotated = array_left_rotation(d, 2)
+print(d)
+print(d_rotated)
